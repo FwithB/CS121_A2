bd33aa2747079eb4399c5630773ef64fdf0f77f4
Jackson Yang <88069745+FwithB@users.noreply.github.com>
2023-04-27 16:56:29 -0700

    Update addition.py

--- addition.py
+++ addition.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from utils.response import Response
 
-
+#before to dig the data, first prepare the tokenize function
 
 def load_stopwords(file_path):
     stopwords_set = set()
@@ -31,7 +31,33 @@ def word_tk(text):
     return text_words
   
   
-  
+# start to check the page that contain the longest amount of text
+
+def read_longest_page(file_path):
+    with open(file_path, "r") as file:
+        lines = [line.strip() for line in file if line.strip()]
+
+    if len(lines) < 2:
+        return None, 0
+    else:
+        return lines[0], int(lines[1])
+
+    
+def write_longest_page(file_path, url, count):
+    with open(file_path, "w") as file:
+        file.write(f"{url}\n{count}")
+
+        
+def longestPage(url, text):
+    tokens = word_tk(text)
+    token_count = len(tokens)
+
+    file_path = "longest.txt"
+    prev_url, prev_count = read_longest_page(file_path)
+
+    if token_count > prev_count:
+        write_longest_page(file_path, url, token_count)
+
   
   
   
